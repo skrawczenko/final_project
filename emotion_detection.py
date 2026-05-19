741819ad6a79c5c09e32e8581f585806567917c0
Serge <skravchenko@Serges-MacBook-Air.local>
2026-05-19 23:42:50 +0300

    emotion4

--- emotion_detection.py
+++ emotion_detection.py
@@ -8,5 +8,7 @@ def emotion_detector(text_to_analyse):
   response = requests.post(url, json = myobj, headers=header)   
   formatted_response = json.loads(response.text)
   emotion = formatted_response['emotionPredictions'][0]['emotion'] 
+  dominant = max(emotion, key=emotion.get)
+  emotion['dominant_emotion'] = dominant
   return emotion 
 
