10fbe34a43087d19d34099980ea92051b4f5898f
Serge <skravchenko@Serges-MacBook-Air.local>
2026-05-20 00:12:30 +0300

    server7

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ def emo_detection():
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
     emotion = responsee
-    print response
+    print(response)
     return "For the given statement, the system response is ", emotion
     # TODO
 
