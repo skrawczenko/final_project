0dcef3cb1d68e693014e1349d39522bb6f2c595b
Serge <skravchenko@Serges-Air.home>
2026-05-20 17:09:28 +0300

    servers1

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from EmotionDetection.emotion_detection import emotion_detector
+"""Flask server for emotion detection."""
 from flask import Flask, render_template, request 
+from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detector")
 @app.route("/emotionDetector")
@@ -16,19 +17,21 @@ def emo_detection():
       "For the given statement, the system response is "
       f"{emotion}"
     ) 
-    # TODO
+    print(
+        f"For the given statement, the system response is "
+        f"'anger': {emotion['anger']}, "
+        f"'disgust': {emotion['disgust']}, "
+        f"'fear': {emotion['fear']}, "
+        f"'joy': {emotion['joy']} and "
+        f"'sadness': {emotion['sadness']}. "
+        f"The dominant emotion is {emotion['dominant_emotion']}."
+)
 
 @app.route("/")
 def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+    # This function initiates the rendering of the main application  page over the Flask channel
     return render_template('index.html')
-    #TODO
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    # This functions executes the flask app and deploys it on localhost:5000
     app.run(host="0.0.0.0", port=5000)
-
-
