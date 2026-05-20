6b8d6a0175a81c6bc9583afee37b6ebf2970f9e0
Serge <skravchenko@Serges-Air.home>
2026-05-20 17:54:49 +0300

    pylint7

--- server.py
+++ server.py
@@ -11,9 +11,6 @@ def emo_detection():
         score for the provided text.
     '''
     text_to_analyze = request.args.get('textToAnalyze')
-    print(text_to_analyze)
-    if not text_to_analyze:
-        return "Bad Request", 400
     response = emotion_detector(text_to_analyze)
     emotion = response
     return (
