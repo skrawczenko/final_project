fc50cdf0944a404ab10a57613b92b8049a4d31fe
Serge <skravchenko@Serges-Air.home>
2026-05-20 17:37:05 +0300

    pylint4

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,7 +6,9 @@ def emotion_detector(text_to_analyse):
   myobj = { "raw_document": { "text": text_to_analyse } } 
   header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
   response = requests.post(url, json = myobj, headers=header)   
+  print(response.status_code)
   if response.status_code == 400:
+      print("Invalid text! Please try again!.")
       emotion = {'anger': None, 'disgust': None, 'fear': None, 'joy': None, 'sadness': None, 'dominant_emotion': 'None'}
   elif response.status_code == 200:
       formatted_response = json.loads(response.text)
